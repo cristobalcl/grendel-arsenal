42deaa7ed0bb7224c664a02ba8f07ddfd2406df9
grendel.ccl@gmail.com <grendel.ccl@gmail.com@8925e529-0f04-70ef-0e6f-1b39e8e2fb27>
2011-01-31 11:17:11 +0000

    Fixes.

--- templates/python/HelloWorld.py
+++ templates/python/HelloWorld.py
@@ -5,23 +5,16 @@ import optparse
 
 
 version = "0.0.1" 
-verbose = False
-quiet = False
 
 
 def processCommandLine():
   parser = optparse.OptionParser(usage="%prog [options] arg", version="%prog "+version)
-  parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=verbose)
-  parser.add_option("-q", "--quiet", action="store_false", dest="quiet", default=quiet)
+  parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=True)
 
   (options, args) = parser.parse_args()
 
   if len(args) != 1:
     parser.error("incorrect number of arguments")
-  if options.verbose:
-    verbose = True
-  if options.quiet:
-    quiet = True
 
   return (options, args)
 
