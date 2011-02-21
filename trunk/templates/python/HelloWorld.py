4f904c9cdd0cee98f6ade8777f721f082e9adc22
grendel.ccl <grendel.ccl@8925e529-0f04-70ef-0e6f-1b39e8e2fb27>
2011-02-21 11:34:26 +0000

    Folling Python guidelines.

--- trunk/templates/python/HelloWorld.py
+++ trunk/templates/python/HelloWorld.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""Abstract.
+
+Description.
+"""
+
+__author__ = "Cristóbal Carnero Liñán"
+__copyright__ = "Copyright 2011, Cristóbal Carnero Liñán"
+__credits__ = ["Cristóbal Carnero Liñán", ] # Code writers, bug reporters, suggestions, ...
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Cristóbal Carnero Liñán"
+__email__ = "grendel.ccl@gmail.com"
+__status__ = "Development" # "Prototype", "Development", "Production"
 
 import sys 
 import optparse
 
 
-version = "0.0.1" 
-
-
 def processCommandLine():
-  parser = optparse.OptionParser(usage="%prog [options] arg", version="%prog "+version)
+  parser = optparse.OptionParser(usage="%prog [options] arg", version="%prog "+__version__)
+  #parser.add_option("-u", "--url", dest="url", default="http://grendel-arsenal.googlecode.com", help="URL")
   parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=True)
 
   (options, args) = parser.parse_args()
