a6d892c12d5e97533aaafbd4c75f8cf324398d19
grendel.ccl@gmail.com <grendel.ccl@gmail.com@8925e529-0f04-70ef-0e6f-1b39e8e2fb27>
2013-01-10 13:20:15 +0000

    Script to convert CBR, CBZ,... files to PDF. Needs 'unp' and 'convert' commands.

---
+++ trunk/bin/cbx2pdf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+#set -e # Exit if error
+set -u # Exit if a variable was not set
+
+here=`pwd`
+
+cbr=${here}/$1
+
+tmp=`mktemp -d`
+mkdir -p "${tmp}"
+
+cd "${tmp}"
+
+unp "${cbr}"
+
+rm "$(basename $cbr)"
+
+if [ "`find . ! -name . -prune -type f`" ]
+then
+        convert `find . ! -name . -prune -type f` "${cbr}.pdf"
+fi
+
+find -maxdepth 1 -mindepth 1 -type d | while read BOOK; do
+        pushd .
+        cd "$BOOK"
+        convert * "$here/$BOOK.pdf"
+        popd
+done
+
+cd $here
+
+rm -rf ${tmp}
